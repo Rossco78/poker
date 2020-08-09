7fa1878674bd66cf2fb9734ce768c2f18ce33f57
Ross <rossco.king78@outlook.com>
2020-08-10 07:57:54 +0930

    first python file

---
+++ count-hands.py
@@ -0,0 +1,12 @@
+# count poker hands
+
+# 1. open poker data file for reading
+pokerFile = open("count-hands.py", 'rU')
+
+totalCount = 0 # 2. create variable to hold the count -  initialize it.
+
+# 3. step through each line of the file
+for line in pokerFile:
+    totalCount = += 1
+
+print("Toatal hands in file: " totalCount)
